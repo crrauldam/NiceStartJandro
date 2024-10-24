11d5bcd133f84504f0f62372108a4c4259101aec
Alejandro Pérez <jandropehe4@gmail.com>
2024-10-24 10:42:36 +0200

    added Glide

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
+
 # NiceStart
+
+---
+
+<img loading="lazy" src="Images/NiceStart.png" width="100%">
+
+# Login
+
+---
+
+
+# Signup
+
+---
+
 
 
 
--- app/src/main/java/com/aperez/nicestart/Login.java
+++ app/src/main/java/com/aperez/nicestart/Login.java
@@ -1,14 +1,20 @@
 package com.aperez.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
@@ -17,6 +23,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        ImageView logo = findViewById(R.id.logo1);
+
+        Glide.with(this)
+//                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+                .load(R.drawable.logo)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.white)))
+                .into(logo);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
